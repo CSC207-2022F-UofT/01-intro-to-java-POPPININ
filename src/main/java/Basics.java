4b3e6b86ccf91f42a95239cb20d0ce1777d7c01f
Raghav Arora <ragaro214@gmail.com>
2022-10-01 14:21:19 -0400

    completed all exercises and all test cases passing

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,6 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
+        System.out.println("Hello World!");
 
 
 
@@ -62,6 +63,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +101,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i);
+        }
 
 
     }
@@ -143,7 +148,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
-
+        String[] words = to_split.split(" ");
+        for (String word: words){
+            ret.append(word.charAt(0));
+        }
         return ret.toString();
     }
 
@@ -162,15 +170,21 @@ public class Basics {
      * @return       The sum of all integers at odd indices in arr
      */
     public static int oddSum(int[] arr) {
+        if (arr.length <= 1) {
+            return 0;
+        }
         int current_sum = 0;
-
         /* TODO (Task 5): Complete this method body using a for-loop.
          *                You can find the length of an array by using the
          *                .length attribute (e.g. arr.length)
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
-
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 1) {
+                current_sum += arr[i];
+            }
+        }
         return current_sum;
     }
 
